1659388881934950b0ffa60aa2a46e2f7198ac64
jpasner <jakepasner@gmail.com>
2023-12-10 18:07:38 -0800

    More cleanup

--- my_pandas_profile.py
+++ my_pandas_profile.py
@@ -4,12 +4,12 @@ from pandas_profiling import ProfileReport
  
 
 # os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
-os.chdir('/home/adh/_projects/eWRIMS_parties')
+os.chdir('<dataLocation>')
 cwd = os.getcwd()
  
 
 # Full_list_InFile = os.path.join(cwd, "water-rights-parties.csv")
-Full_list_InFile = os.path.join(cwd, "EWRIMS_PARTY_DUP.xlsx") # Don't directly link to specific folders!!!!
+Full_list_InFile = os.path.join(cwd, "exampleFile.xlsx") # Don't directly link to specific folders!!!!
  
 ##################  reading in full non-QAed data
 full_data = pd.read_excel(Full_list_InFile)
@@ -19,4 +19,4 @@ full_data = pd.read_excel(Full_list_InFile)
 profile = ProfileReport(full_data, title="Pandas Profiling Report", explorative=True)
 # profile.to_widgets()
 # profile.to_notebook_iframe()
-profile.to_file("EWRIMS_PARTY_DUP-report.html")
+profile.to_file("your_report.html")
